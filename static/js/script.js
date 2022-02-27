18f87607283abb8598638a21e1b365cc074f1b75
Resford <resford1@gmail.com>
2022-02-26 17:29:17 -0700

    project completed by days

---
+++ static/js/script.js
@@ -0,0 +1,19 @@
+//Your Age in Days
+
+function ageInDays() {
+    let birthDate = new Date(prompt("What year were you born? MM-DD-YYY" ));
+    let today = new Date();
+    let currentDate = new Date(today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate());
+    let differenceInTime = currentDate.getTime() - birthDate.getTime();
+    let differenceInDays = differenceInTime / (1000 * 3600 * 24);
+    console.log(differenceInDays)
+    let h1 = document.createElement('h1');
+    let textAnswer = document.createTextNode("You are " + differenceInDays + " days old");
+    h1.setAttribute("id", "ageInDays");
+    h1.appendChild(textAnswer);
+    document.getElementById("flex-box-result").appendChild(h1);
+}
+
+function reset() {
+    document.getElementById('ageInDays').remove();
+}
